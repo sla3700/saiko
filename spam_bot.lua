a000fa02c09d6c94ac782b86e3bf55e49632429c
sla3700 <46056225+sla3700@users.noreply.github.com>
2019-01-14 03:10:24 -0200

    Update spam_bot.lua

--- spam_bot.lua
+++ spam_bot.lua
@@ -14,14 +14,14 @@ coroutine.wrap(function()
     client.connect(account[1].user, account[1].pass)
     fromage.connect(account[0].user, account[0].pass)
      local again = 0
-	timer.setInterval(60000, function()
+	timer.setInterval(60000, function() -- change the "60000" if you want more fast/slow (that are miliseconds)
 		if again < 0 or again > os.time() then return end
 
 	  coroutine.wrap(function()
   again = -1
   print(client.answerTopic(account[1].msg, location))
   print(fromage.answerTopic(account[0].msg, location))
-			again = os.time() + 60
+			again = os.time() + 60 -- change the "60" if you want more fast/slow (that are seconds)
         end)()
 	end)
 end)()
