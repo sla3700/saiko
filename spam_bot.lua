e4f7bfdac23062e9b14b4cd135762e3f150b1865
sla3700 <46056225+sla3700@users.noreply.github.com>
2019-01-14 03:08:47 -0200

    Added Spam_Bot

---
+++ spam_bot.lua
@@ -0,0 +1,27 @@
+local api = require("fromage")
+local fromage = api()
+local client = api()
+local enum = client.enumerations()
+
+local location = { f = 5, t = 917668 } -- f - Section and T - Topic
+local account = {
+	[0] = { user = "name#000", pass = "pass", msg = "message"},
+	[1] = { user = "name#0000", pass = "pas", msg = "message"}
+
+}
+
+coroutine.wrap(function()   
+    client.connect(account[1].user, account[1].pass)
+    fromage.connect(account[0].user, account[0].pass)
+     local again = 0
+	timer.setInterval(60000, function()
+		if again < 0 or again > os.time() then return end
+
+	  coroutine.wrap(function()
+  again = -1
+  print(client.answerTopic(account[1].msg, location))
+  print(fromage.answerTopic(account[0].msg, location))
+			again = os.time() + 60
+        end)()
+	end)
+end)()
